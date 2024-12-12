a566a79db65d031d94cd1e3875963a41ea45a248
whz1gud <vyt.itzip@gmail.com>
2024-12-12 14:32:39 +0200

    Create Dto's for payment and minor changes in the PaymentController

--- PoS_Placeholder.Server/Controllers/PaymentController.cs
+++ PoS_Placeholder.Server/Controllers/PaymentController.cs
@@ -1,53 +1,45 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
-using PoS_Placeholder.Server.Data;
+using PoS_Placeholder.Server.Models.Dto;
 using Stripe;
 
 namespace PoS_Placeholder.Server.Controllers;
 
 [ApiController]
 [Route("/api/payments")]
-public class PaymentController
+public class PaymentController : ControllerBase
 {
-    private readonly ApplicationDbContext _db;
     private readonly IConfiguration _configuration;
 
-    public PaymentController(ApplicationDbContext db, IConfiguration configuration)
+    public PaymentController(IConfiguration configuration)
     {
-        _db = db;
         _configuration = configuration;
     }
 
-    [HttpPost]
+    [HttpPost("makepayment")]
     [Authorize]
-    public async Task<IActionResult> MakePayment()
+    public async Task<IActionResult> MakePayment([FromBody] PaymentRequestDto paymentRequestDto)
     {
-        #region Create Payment Intent
-        
         StripeConfiguration.ApiKey = _configuration["StripeSettings:SecretKey"];
-        // will need to get it here correctly from the front-end for that specific order, by that user
-        decimal totalAmount = 10.50m;
         
         var options = new PaymentIntentCreateOptions
         {
-            Amount = (long)Math.Round(totalAmount * 100m),
+            Amount = (long)Math.Round(paymentRequestDto.TotalAmount * 100m),
             Currency = "usd",
-            //this wasn't in the video, instead there was PaymentMethodTypes with "card" inside, do we need it?
             AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions
             {
                 Enabled = true,
             },
-            //Do we need this methodTypes? It wasn't in the boiler plate code that I copied from stripe docs, but in video it was present
-            // since the video is older pehaps we no longer need this PaymentMethodTypes, if the newest version does not have it?
-            // PaymentMethodTypes = new List<string>
-            // {
-            //     "card"
-            // }
         };
         var service = new PaymentIntentService();
-        var response = service.Create(options);
+        var paymentIntentResponse = service.Create(options);
+
+        var paymentResponseDto = new PaymentResponseDto
+        {
+            ClientSecret = paymentIntentResponse.ClientSecret,
+            PaymentIntentId = paymentIntentResponse.Id
+        };
         
-        #endregion
+        return Ok(paymentResponseDto);
     }
 }
---
+++ PoS_Placeholder.Server/Models/Dto/PaymentResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace PoS_Placeholder.Server.Models.Dto;
+
+public class PaymentResponseDto
+{
+    public string ClientSecret { get; set; }
+    public string PaymentIntentId { get; set; }
+}
