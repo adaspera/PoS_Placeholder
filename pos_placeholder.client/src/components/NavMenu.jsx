9c73a8108b14c9485045f32856f350f70f14ddac
whz1gud <vyt.itzip@gmail.com>
2024-12-15 19:54:39 +0200

    Implemented refund endpoint

--- pos_placeholder.client/src/components/NavMenu.jsx
+++ pos_placeholder.client/src/components/NavMenu.jsx
@@ -14,41 +14,41 @@ const NavMenu = ({ onLogout }) => {
     };
 
     return (
-    <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom bg-white">
-            <NavbarBrand tag={Link} to="/">
-                <img src="/ico.png" alt="PineApp" style={{height: '50px'}}/>
-            </NavbarBrand>
-            <NavbarToggler onClick={() => setCollapsed(!isCollapsed)} className="mr-2" />
-            <Collapse isOpen={!isCollapsed} navbar>
-                <Nav className="me-auto" navbar>
-                    <NavLink tag={Link} to="/business" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
-                        <i className="bi-briefcase" style={{fontSize: '20px'}}/>
-                        <div>Business</div>
-                    </NavLink>
-                    <NavLink tag={Link} to="/tax" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
-                        <i className="bi-percent" style={{fontSize: '20px'}}/>
-                        <div>Discount</div>
-                    </NavLink>
-                    <NavLink tag={Link} to="/products" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
-                        <i className="bi-box-seam" style={{fontSize: '20px'}}/>
-                        <div>Products</div>
-                    </NavLink>
-                    <NavLink tag={Link} to="/orders" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
-                        <i className="bi-receipt" style={{fontSize: '20px'}}/>
-                        <div>Orders</div>
-                    </NavLink>
-                </Nav>
-                <Nav className="ms-auto">
-                    <NavItem
-                        className="text-dark" style={{cursor: "pointer"}}
-                        onClick={handleLogout}>
-                        Logout
-                    </NavItem>
-                </Nav>
-            </Collapse>
-        </Navbar>
-    </header>
+        <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom bg-white">
+                <NavbarBrand tag={Link} to="/">
+                    <img src="/ico.png" alt="Placeholder" style={{height: '50px'}}/>
+                </NavbarBrand>
+                <NavbarToggler onClick={() => setCollapsed(!isCollapsed)} className="mr-2" />
+                <Collapse isOpen={!isCollapsed} navbar>
+                    <Nav className="me-auto" navbar>
+                        <NavLink tag={Link} to="/business" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
+                            <i className="bi-briefcase" style={{fontSize: '20px'}}/>
+                            <div>Business</div>
+                        </NavLink>
+                        <NavLink tag={Link} to="/tax" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
+                            <i className="bi-percent" style={{fontSize: '20px'}}/>
+                            <div>Discount</div>
+                        </NavLink>
+                        <NavLink tag={Link} to="/products" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
+                            <i className="bi-box-seam" style={{fontSize: '20px'}}/>
+                            <div>Products</div>
+                        </NavLink>
+                        <NavLink tag={Link} to="/orders" className="text-dark d-flex flex-column align-items-center" style={{width: "80px"}}>
+                            <i className="bi-receipt" style={{fontSize: '20px'}}/>
+                            <div>Orders</div>
+                        </NavLink>
+                    </Nav>
+                    <Nav className="ms-auto">
+                        <NavItem
+                            className="text-dark" style={{cursor: "pointer"}}
+                            onClick={handleLogout}>
+                            Logout
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </header>
     );
 }
 
--- pos_placeholder.client/src/components/Orders.jsx
+++ pos_placeholder.client/src/components/Orders.jsx
@@ -1,7 +1,20 @@
+import {Button, Col, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, Row, Form, FormGroup} from "reactstrap";
+import {useEffect, useState} from "react";
+import {getCurrency} from "@/helpers/currencyUtils.jsx";
+
 const Orders = () => {
+
+
     return (
-        <h1>Orders</h1>
-    )
-}
+        <Row style={{height: "85vh"}}>
+            <Col className="border rounded shadow-sm m-2 p-2 d-flex flex-column" lg={4}>
+                Order list
+            </Col>
+            <Col className="border rounded shadow-sm m-2 p-2">
+                Specific fetched order ui (when view receipt button is pressed)
+            </Col>
+        </Row>
+    );
+};
 
 export default Orders;
