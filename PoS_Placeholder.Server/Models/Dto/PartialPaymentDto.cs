010699d36fa49644b52076a64ec1b59de355f176
whz1gud <vyt.itzip@gmail.com>
2024-12-18 14:19:53 +0200

    Split check backend endpoint

---
+++ PoS_Placeholder.Server/Models/Dto/PartialPaymentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PoS_Placeholder.Server.Models.Enum;
+
+namespace PoS_Placeholder.Server.Models.Dto;
+
+public class PartialPaymentDto
+{
+    public string? PaymentIntentId { get; set; }
+    public string? GiftCardId { get; set; }
+    [Required]
+    public PaymentMethod Method { get; set; }
+    [Required]
+    public decimal PaidPrice { get; set; }
+}
